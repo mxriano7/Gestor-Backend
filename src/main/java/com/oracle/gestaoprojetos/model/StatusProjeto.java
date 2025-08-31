31fecaeb166cf0ab40b92e3c2f06b61e39355328
Paulo Mariano <you@example.com>
2025-08-31 03:48:38 -0300

    Remove gestao-projetos do controle do Git

--- src/main/java/com/oracle/gestaoprojetos/model/StatusProjeto.java
+++
@@ -1,8 +0,0 @@
-package com.oracle.gestaoprojetos.model;
-
-public enum StatusProjeto {
-    PLANEJADO,
-    EM_ANDAMENTO,
-    CONCLUIDO,
-    ATRASADO
-}
--- src/main/java/com/oracle/gestaoprojetos/model/StatusTarefa.java
+++
@@ -1,8 +0,0 @@
-package com.oracle.gestaoprojetos.model;
-
-public enum StatusTarefa {
-    PENDENTE,
-    EM_ANDAMENTO,
-    CONCLUIDA,
-    ATRASADA
-}
